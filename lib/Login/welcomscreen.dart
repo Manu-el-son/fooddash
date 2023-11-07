200156e1b889f1b16842d32a1a81bc5afc745061
Passah Emmanuel Semenyo Mawunya Kwaku <119499414+Manu-el-son@users.noreply.github.com>
2023-11-07 14:06:49 +0000

    More coding

--- lib/Login/welcomscreen.dart
+++ lib/Login/welcomscreen.dart
@@ -1,1 +1,35 @@
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                // Add login button functionality here
+                // Typically, you would navigate to the login screen.
+              },
+              child: Text('Login'),
+            ),
+            SizedBox(height: 16), // Add some spacing
+            ElevatedButton(
+              onPressed: () {
+                // Add signup button functionality here
+                // Typically, you would navigate to the signup screen.
+              },
+              child: Text('Signup'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,7 +1,21 @@
+// ignore_for_file: use_key_in_widget_constructors, use_build_context_synchronously, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:fooddash/Login/welcomscreen.dart';
+import 'dart:async';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key});
+
+  Future<void> navigateToWelcomeScreen(BuildContext context) async {
+    await Future.delayed(
+        Duration(seconds: 3)); // Simulate a delay, e.g., 3 seconds
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => WelcomeScreen(),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
